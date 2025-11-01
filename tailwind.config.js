6087c9b9ef33b9b3095f74780ffefad2233c9212
Aline Marques <alinem_oliveira@yahoo.com>
2025-11-01 12:16:03 -0300

    Redesigned header and footer

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,27 @@
-export default {
+/** @type {import('tailwindcss').Config} */
+module.exports = {
   content: [
-    './_layouts/**/*.html',
-    './_includes/**/*.html',
-    './_posts/**/*.md',
-    './*.html',
-    './*.md'
-  ]
+    "./**/*.html",
+    "./**/*.md",
+    "./_posts/**/*"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'bg-main': '#2F3A46',
+        'bg-secundary': '#26323E',
+        'text-primary': '#E4E1DC',
+        'text-secundary': '#9AA4AE',
+        'accent': '#C2CBD4',
+        'border': '#3C4853',
+        'icon': '#B7BEC7'
+      },
+      fontFamily: {
+        sans: ['Playfair Display', 'sans-serif'],
+        serif: ['Source Serif Pro', 'serif'],
+        mono: ['JetBrains Mono', 'monospace']
+      }
+    },
+  },
+  plugins: [],
 }
